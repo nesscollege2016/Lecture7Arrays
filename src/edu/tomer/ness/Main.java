39fa0967db99a5f5a23fe8445cf94c4e431020a2
nesscollege2016 <nesscollege2016@gmail.com>
2016-02-25 09:29:30 +0200

    targil kita shorter

--- src/edu/tomer/ness/Main.java
+++ src/edu/tomer/ness/Main.java
@@ -9,39 +9,28 @@ public class Main {
 
         System.out.println("Enter the number of students:");
         int size = s.nextInt();
-        
+        int sum = 0;
         int[] grades = new int[size];
+        int min = grades[0];
+        int max = grades[0];
+
         //now we fill the grades array
         for (int i = 0; i < size; i++) {
             System.out.println("Enter the grade:");
-            grades[i] = s.nextInt();;
-        }
-
-
-        int sum = 0;
-        for (int i = 0; i < size; i++) {
+            grades[i] = s.nextInt();
             sum = sum + grades[i];
+
+            int grade = grades[i];
+            if (grade < min)
+                min = grade;
+
+            if (grade > max)
+                max = grade;
         }
 
         double avg = sum / size;
         System.out.println("The avg is " + avg);
-
-        //calculate the min grade;
-
-        int min = grades[0];
-        for (int i = 0; i < size; i++) {
-            int grade = grades[i];
-            if (grade < min)
-                min = grade;
-        }
         System.out.println("the min is " + min);
-
-        int max = grades[0];
-        for (int i = 0; i < size; i++) {
-            int grade = grades[i];
-            if (grade > max)
-                max = grade;
-        }
         System.out.println("the max is " + max);
     }
 }
