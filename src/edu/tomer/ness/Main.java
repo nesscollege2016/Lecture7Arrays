a1475c9cdcf681f781e24c798f2f324a75639c5d
nesscollege2016 <nesscollege2016@gmail.com>
2016-02-25 09:25:32 +0200

    targil kita

--- src/edu/tomer/ness/Main.java
+++ src/edu/tomer/ness/Main.java
@@ -7,19 +7,41 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        //1) declare an array with a size:
-        int[]  numbers = new int[3];
-
-        // setting all the values in the array:
-        for (int i = 0; i < numbers.length; i++) {
-            // setting values in the array:
-            System.out.println("Enter the "+ i +" number");
-            numbers[i] = s.nextInt();
+        System.out.println("Enter the number of students:");
+        int size = s.nextInt();
+        
+        int[] grades = new int[size];
+        //now we fill the grades array
+        for (int i = 0; i < size; i++) {
+            System.out.println("Enter the grade:");
+            grades[i] = s.nextInt();;
         }
 
-        // reading all the values from the array:
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println("the value of nums["+i+"] = " +numbers[i]);
+
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum = sum + grades[i];
         }
+
+        double avg = sum / size;
+        System.out.println("The avg is " + avg);
+
+        //calculate the min grade;
+
+        int min = grades[0];
+        for (int i = 0; i < size; i++) {
+            int grade = grades[i];
+            if (grade < min)
+                min = grade;
+        }
+        System.out.println("the min is " + min);
+
+        int max = grades[0];
+        for (int i = 0; i < size; i++) {
+            int grade = grades[i];
+            if (grade > max)
+                max = grade;
+        }
+        System.out.println("the max is " + max);
     }
 }
