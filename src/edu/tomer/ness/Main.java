ffe3ad84bc3740e922ce43e6c9c5f369e7d019e5
nesscollege2016 <nesscollege2016@gmail.com>
2016-02-25 10:15:18 +0200

    targil dugma

--- src/edu/tomer/ness/Main.java
+++ src/edu/tomer/ness/Main.java
@@ -6,24 +6,28 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter the number of students:");
-        int sum = 0;
-        int[] grades = new int[s.nextInt()];
-        int min = 100;
-        int max = 0;
 
-        //now we fill the grades array
-        for (int i = 0; i < grades.length; i++) {
+        //get the array size from the user:
+        System.out.println("How many numbers?");
+        int size = s.nextInt();
 
-            System.out.println("Enter the grade:");
-            grades[i] = s.nextInt();
-            sum += grades[i];
-            min = grades[i] < min ? grades[i] : min;
-            max = grades[i] > max ? grades[i] : max;
+        //define the array
+        int[] numbers = new int[size];
+
+        //fill the array:
+        for (int i = 0; i < size; i++) {
+            System.out.println("Enter a number");
+            numbers[i] = s.nextInt();
         }
 
-        System.out.println("The avg is " + sum / grades.length);
-        System.out.println("the min is " + min);
-        System.out.println("the max is " + max);
+        //iterate over the array and calc arr[i] += arr[i+1]
+        for (int i = 0; i < size - 1; i++) {
+            numbers[i] += numbers[i+1];
+        }
+
+        //print the array:
+        for (int i = 0; i < size; i++) {
+            System.out.println(numbers[i]);
+        }
     }
 }
